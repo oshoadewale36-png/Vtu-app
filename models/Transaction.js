fb40ebfc5ced2165c995e96fd14d6d0c99a2c6ab
oshoadewale36-png <oshoadewale36@gmail.com>
2026-05-17 09:52:11 -0700

    added transaction history

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,31 +1,13 @@
 const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
-    userId: {
-        type: String,
-        required: true
-    },
-    type: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: String
-    },
-    network: {
-        type: String
-    },
-    plan: {
-        type: String
-    },
-    amount: {
-        type: Number,
-        required: true
-    },
-   
-    date: {
-        type: Date,
-        default: Date.now
-    }
-});
+    userId: String,
+    type: String,
+    amount: Number,
+    status: String,
+    phone: String,
+    network: String
+}, { timestamps: true });
+
+    date:
 module.exports = mongoose.model("Transaction", transactionSchema);
