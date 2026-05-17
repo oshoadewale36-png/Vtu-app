45ebcf712ff467b7dc7cb378cf24cb132598318f
oshoadewale36-png <oshoadewale36@gmail.com>
2026-05-17 09:27:20 -0700

    fixed userId type

--- models/Wallet.js
+++ models/Wallet.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const walletSchema = new mongoose.Schema({
     userId: {
-        type: mongoose.Schema.Types.ObjectId,
+        type: String,
         required: true,
         unique: true
     },
